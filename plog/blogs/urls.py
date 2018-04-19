e31127af874ad8ee041e96acc1601c3cc8f74644
Isaiah Jan Caracol <isaiahnavasquez@gmail.com>
2018-04-19 21:09:37 +0800

    finished view blog template

--- plog/blogs/urls.py
+++ plog/blogs/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blogs'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:blog_id>/', views.view_blog, name='view'),
 ]
--- plog/blogs/views.py
+++ plog/blogs/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blogs/index.html')
+
+def view_blog(request, blog_id):
+    return render(request, 'blogs/view.html')
