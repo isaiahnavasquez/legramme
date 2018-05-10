f16d589a1e975fff6020fe28dc34fbff948ec4d0
Isaiah Jan Caracol <isaiahnavasquez@gmail.com>
2018-05-10 14:59:50 +0800

    created api/project and class-based serializers based from existing models on blog/

---
+++ api/views.py
@@ -0,0 +1,107 @@
+from blogs.models import Blog, Category, Profile
+from api.serializers import BlogSerializer, CategorySerializer, HashtagSerializer, UserSerializer, ProfileSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from django.contrib.auth.models import User
+
+class BlogList(APIView):
+    """
+    List Blogs or Create new
+    """
+
+    # authentication filter
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get(self, request, format=None):
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class BlogDetail(APIView):
+    """
+    CRUD with a Blog instance
+    """
+
+    # authentication filter
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_object(self, pk):
+        try:
+            return Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        blog = self.get_object(pk)
+        serializer = BlogSerializer(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserList(APIView):
+    """
+    List User information
+    """
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+class UserDetail(APIView):
+    """
+    Retrieve single User instance
+    """
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+class CategoryList(APIView):
+    """
+    Retrieve Category records
+    """
+
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+class ProfileDetail(APIView):
+    """
+    Retrieve a user's extended model reference
+    """
+
+    def get_object(self, pk):
+        try:
+            return Profile.objects.get(user=pk)
+        except Profile.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
